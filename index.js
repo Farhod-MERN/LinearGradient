b69b4889068be048ce20873a4944da39b4c3fb64
Farhod <mrfarhod58@gmail.com>
2022-12-14 16:34:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const values = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+
+const body = document.querySelector("body");
+const div = document.querySelector(".div");
+const textValue = document.querySelector(".text-value");
+
+//random
+
+function getGradient() {
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.trunc(Math.random() * values.length);
+    console.log(values[randomNumber]);
+
+    color += values[randomNumber];
+  }
+  return color;
+}
+// console.log(color1, color2);
+
+function setGradient() {
+     const color1 = getGradient();
+     const color2 = getGradient();  
+     const randomDeg = Math.trunc(Math.random() * 360);
+  const bgColor = `linear-gradient(${randomDeg}deg, ${color1}, ${color2})`;
+  console.log(bgColor);
+
+  body.style.background = bgColor
+  textValue.textContent = bgColor
+}
+setGradient()
+div.addEventListener("click", ()=>{
+     
+     setGradient()
+})
